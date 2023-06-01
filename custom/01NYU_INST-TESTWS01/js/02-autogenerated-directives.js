c5a256690b1840d6e56f50e34f7d44bad20fab23
David <da70@nyu.edu>
2023-06-01 17:20:15 -0400

    custom/01NYU_INST-TESTWS01/js/: split 02-directives.js into 02-autogenerated-directives and 03-poc-directives

---
+++ custom/01NYU_INST-TESTWS01/js/02-autogenerated-directives.js
@@ -0,0 +1,7 @@
+const exclude = [
+    'prmExploreFooterAfter',
+    'prmExploreMainAfter',
+    'prmSearchAfter',
+    'prmSilentLoginAfter',
+]
+
--- custom/01NYU_INST-TESTWS01/js/03-poc-directives.js
+++ custom/01NYU_INST-TESTWS01/js/03-poc-directives.js
@@ -127,4 +127,3 @@ app.component( 'prmSilentLoginAfter', {
             );
     },
 } );
-
