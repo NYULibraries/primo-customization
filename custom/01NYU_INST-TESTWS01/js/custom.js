fb403db65b48700590728d6817fcbafe2620859c
David <da70@nyu.edu>
2023-06-01 11:45:13 -0400

    Update CloudFront domain for localhost CDN

--- custom/01NYU_INST-TESTWS01/js/custom.js
+++ custom/01NYU_INST-TESTWS01/js/custom.js
@@ -7,7 +7,7 @@
         const cdnUrls = {
             // CloudFront distribution: https://us-east-1.console.aws.amazon.com/cloudfront/v3/home?region=us-east-1#/distributions/E1CAY9LEN0VBFY
             // ...with origin: cdn-local-da70.library.nyu.edu.s3.us-east-1.amazonaws.com
-            'localhost'                           : 'https://d2udqvw2vzqson.cloudfront.net/primo-customization/01NYU_INST-TESTWS01',
+            'localhost'                           : 'https://d27uw5gej4v6yt.cloudfront.net/primo-customization/01NYU_INST-TESTWS01',
             'sandbox02-na.primo.exlibrisgroup.com': 'https://cdn-dev.library.nyu.edu/primo-customization/01NYU_INST-TESTWS01',
         }
 
