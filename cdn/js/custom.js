deddb78d43968719d960001d5d656911c8175fce
David <da70@nyu.edu>
2023-06-09 16:40:03 -0400

    cdn/js/custom.js: restore CDN URL stuff

--- cdn/js/custom.js
+++ cdn/js/custom.js
@@ -1,3 +1,12 @@
+const cdnUrls = {
+    // CloudFront distribution: https://us-east-1.console.aws.amazon.com/cloudfront/v3/home?region=us-east-1#/distributions/E1CAY9LEN0VBFY
+    // ...with origin: cdn-local-da70.library.nyu.edu.s3.us-east-1.amazonaws.com
+    'localhost'                           : 'https://d27uw5gej4v6yt.cloudfront.net/primo-customization/01NYU_INST-TESTWS01',
+    'sandbox02-na.primo.exlibrisgroup.com': 'https://cdn-dev.library.nyu.edu/primo-customization/01NYU_INST-TESTWS01',
+}
+
+const cdnUrl = cdnUrls[ window.location.hostname ] || 'https://d2udqvw2vzqson.cloudfront.net/primo-customization/01NYU_INST-TESTWS01';
+
 function injectStylesheetLinkElement() {
     const link = document.createElement( 'link' );
     link.type = 'text/css';
