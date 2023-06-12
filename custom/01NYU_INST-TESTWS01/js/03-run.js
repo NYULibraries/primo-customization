14690a8ffe1a01426cdff0a8548bd918b40972e4
David <da70@nyu.edu>
2023-06-12 16:10:49 -0400

    cdn/: move assets to new primo-customization/01NYU_INST-TESTWS01/ and move inject stylesheet link element code from CDN to custom package

--- custom/01NYU_INST-TESTWS01/js/03-run.js
+++ custom/01NYU_INST-TESTWS01/js/03-run.js
@@ -1,8 +1,23 @@
 // Append <script src='[OUR CUSTOMIZATION SCRIPT]'> tag to the end of <body>.
-app.run( injectScriptTagForCDNCustomJS );
+app.run( injectCDNResourceTags );
+
+function injectCDNResourceTags() {
+    injectLinkTagForCDNCustomCSS()
+    injectScriptTagForCDNCustomJS();
+}
 
 function injectScriptTagForCDNCustomJS() {
     const script = document.createElement( 'script' );
     script.setAttribute( 'src', `${ cdnUrl }/js/custom.js` );
     document.body.appendChild( script );
+}
+
+function injectLinkTagForCDNCustomCSS() {
+    const link = document.createElement( 'link' );
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+
+    document.head.appendChild( link );
+
+    link.href = `${ cdnUrl }/css/custom.css`;
 }
