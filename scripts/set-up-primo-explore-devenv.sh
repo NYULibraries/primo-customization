4bd6850cd4a603c31f5c0ef6b6d4e080bc52a28a
David <da70@nyu.edu>
2023-06-22 17:41:24 -0400

    Check primo-explore-devenv/ into this repo instead of relying developer to set it up [+]
    ...and update README.md and scripts/set-up-primo-explore-devenv.sh (which now just serves as documentation of for how `primo-explore-devenv/` was made)

--- scripts/set-up-primo-explore-devenv.sh
+++ scripts/set-up-primo-explore-devenv.sh
@@ -14,28 +14,35 @@ if [ -e $PRIMO_EXPLORE_DEVENV_LOCAL ]; then
   fi
 fi
 
-PRIMO_EXPLORE_DEVENV_REPO='git@github.com:NYULibraries/primo-explore-devenv.git'
-PRIMO_EXPLORE_DEVENV_COMMIT='77835021e9c1b1e15c8353b17901170961edd36d'
+PRIMO_EXPLORE_DEVENV_REPO='git@github.com:ExLibrisGroup/primo-explore-devenv.git'
+PRIMO_EXPLORE_DEVENV_COMMIT='ad78ad5d8db2e727f612657eca5d222e89366110'
 
 git clone $PRIMO_EXPLORE_DEVENV_REPO $PRIMO_EXPLORE_DEVENV_LOCAL
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-cd $PRIMO_EXPLORE_DEVENV_LOCAL_CUSTOM || exit 1
+cd $PRIMO_EXPLORE_DEVENV_LOCAL || exit 1
 
 git checkout $PRIMO_EXPLORE_DEVENV_COMMIT >/dev/null
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
+rm -fr .git/
+
+# This inline replace sed command is written for execution on a Mac.  It might
+# require a slight adjustment to the -i option in a Linux environment.
+sed -i '' 's@http://your-server:your-port@https://sandbox02-na.primo.exlibrisgroup.com:443@g' ./gulp/config.js
+
+cd $PRIMO_EXPLORE_DEVENV_LOCAL_CUSTOM || exit 1
+
 ln -s ../../../custom/01NYU_INST-TESTWS01 .
 
 cd $PRIMO_EXPLORE_DEVENV_LOCAL || exit 1
 
-rm -fr .git/
+yarn install
 
-echo -e "\n*******\n\nTo complete setup, run \`yarn install\` inside $PRIMO_EXPLORE_DEVENV_LOCAL using Node 16.17.0 or higher.\n"
 
 
 
