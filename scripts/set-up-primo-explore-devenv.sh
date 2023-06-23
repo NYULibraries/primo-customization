13623a5e7cf5a7987eee6c3bd3b1994f1db6816c
David <da70@nyu.edu>
2023-06-23 13:18:29 -0400

    primo-explore-devenv/: replace buggy ExLibris .gitignore and check in custom/ symlink; update script and yarn.lock

--- scripts/set-up-primo-explore-devenv.sh
+++ scripts/set-up-primo-explore-devenv.sh
@@ -3,7 +3,6 @@
 ROOT=$( cd "$(dirname "$0")" ; cd ..; pwd -P )
 
 PRIMO_EXPLORE_DEVENV_LOCAL=$ROOT/primo-explore-devenv
-PRIMO_EXPLORE_DEVENV_LOCAL_CUSTOM=$PRIMO_EXPLORE_DEVENV_LOCAL/primo-explore/custom/
 
 if [ -e $PRIMO_EXPLORE_DEVENV_LOCAL ]; then
   read -p "$PRIMO_EXPLORE_DEVENV_LOCAL already exists.  Overwrite it? " -r
@@ -31,13 +30,24 @@ fi
 
 rm -fr .git/
 
+# Set PROXY_SERVER.
 # This inline replace sed command is written for execution on a Mac.  It might
 # require a slight adjustment to the -i option in a Linux environment.
 sed -i '' 's@http://your-server:your-port@https://sandbox02-na.primo.exlibrisgroup.com:443@g' ./gulp/config.js
 
-cd $PRIMO_EXPLORE_DEVENV_LOCAL_CUSTOM || exit 1
+# ExLibris .gitignore has bugs in it: https://github.com/NYULibraries/primo-customization/blob/4bd6850cd4a603c31f5c0ef6b6d4e080bc52a28a/primo-explore-devenv/.gitignore#L1-L2
+# ...should be "primo-explore/custom/*" and "primo-explore/custom/*".
+# Also, we need to remove the rule for custom/ as we actually do want to check in
+# the symlimks.
+cat << EOF > .gitignore
+primo-explore/tmp/
+!primo-explore/tmp/.gitignore
+EOF
 
-ln -s ../../../custom/01NYU_INST-TESTWS01 .
+# Replace empty custom/ directory with symlink to our custom/
+cd $PRIMO_EXPLORE_DEVENV_LOCAL/primo-explore/ || exit 1
+rm -fr custom/
+ln -s ../../custom/ .
 
 cd $PRIMO_EXPLORE_DEVENV_LOCAL || exit 1
 
