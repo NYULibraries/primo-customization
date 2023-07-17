9491e4ea2f46786ca16724fece55a26f990b55da
David <da70@nyu.edu>
2023-07-17 17:19:21 -0400

    custom/01NYU_NETWORK/js/01-config.js: upgrade CDN URL autoconfiguration to multi-view

--- custom/01NYU_NETWORK/js/01-config.js
+++ custom/01NYU_NETWORK/js/01-config.js
@@ -1,10 +1,8 @@
-const cdnUrls = {
-    'localhost'                           : 'http://localhost:3000/primo-customization/01NYU_INST-TESTWS01',
-    'nyu.primo.exlibrisgroup.com'         : 'https://cdn-dev.library.nyu.edu/primo-customization/01NYU_INST-TESTWS01',
-    'sandbox02-na.primo.exlibrisgroup.com': 'https://d290kawcj1dea9.cloudfront.net/primo-customization/01NYU_INST-TESTWS01',
-}
+const searchParams = new URLSearchParams( window.location.search );
+const vid = searchParams.get( 'vid' );
+const cdnUrl = getCdnUrl( vid );
 
-const cdnUrl = cdnUrls[ window.location.hostname ] || 'http://localhost:3000/primo-customization/01NYU_INST-TESTWS01';
+console.log( `[DEBUG] cdnUrl = ${ cdnUrl }` );
 
 // This is necessary to allow the `templateURL` method to fetch cross-domain
 // from the CDN.
@@ -19,3 +17,34 @@ app.config( function ( $sceDelegateProvider ) {
         ],
     );
 } );
+
+function getCdnUrl( vid ) {
+    const view = parseViewDirectoryName( vid );
+
+    const cdnUrls = {
+        'localhost': {
+            '01NYU_INST:NYU'     : 'http://localhost:3000/primo-customization',
+            '01NYU_INST:NYU_DEV' : 'http://localhost:3000/primo-customization',
+            '01NYU_INST:TESTWS01': 'http://localhost:3000/primo-customization',
+        },
+        'nyu.primo.exlibrisgroup.com': {
+            '01NYU_INST:NYU'     : 'https://cdn.library.nyu.edu/primo-customization',
+            '01NYU_INST:NYU_DEV' : 'https://cdn-dev.library.nyu.edu/primo-customization',
+            '01NYU_INST:TESTWS01': 'https://cdn-dev.library.nyu.edu/primo-customization',
+        },
+        'sandbox02-na.primo.exlibrisgroup.com': {
+            '01NYU_INST:NYU'     : 'https://d290kawcj1dea9.cloudfront.net/primo-customization',
+            '01NYU_INST:NYU_DEV' : 'https://d290kawcj1dea9.cloudfront.net/primo-customization',
+            '01NYU_INST:TESTWS01': 'https://d290kawcj1dea9.cloudfront.net/primo-customization',
+        },
+    };
+
+    const baseUrl = cdnUrls[ window.location.hostname ][ vid ] ||
+                    cdnUrls[ 'localhost' ][ '01NYU_INST:NYU_DEV' ];
+
+    return `${ baseUrl }/${ view }`;
+}
+
+function parseViewDirectoryName( vid ) {
+    return vid.replaceAll( ':', '-' );
+}
