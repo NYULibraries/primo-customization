5590c459aae22eea616ab8b49542481144d40ae0
David <da70@nyu.edu>
2023-07-24 11:44:28 -0400

    cdn/primo-customization/01NYU_INST-*/js/custom.js: slim down LibKey configuration override objects

--- cdn/primo-customization/01NYU_INST-NYU_DEV/js/custom.js
+++ cdn/primo-customization/01NYU_INST-NYU_DEV/js/custom.js
@@ -1,37 +1,16 @@
 const browzineConfig = {
-    journalCoverImagesEnabled: true,
+    journalBrowZineWebLinkText : '[THIS IS AN OVERRIDE FROM CDN custom.js] View Journal Contents',
 
-    journalBrowZineWebLinkTextEnabled: true,
-    journalBrowZineWebLinkText       : '[THIS IS AN OVERRIDE FROM CDN custom.js] View Journal Contents',
+    articleBrowZineWebLinkText : '[THIS IS AN OVERRIDE FROM CDN custom.js] View Issue Contents',
 
-    articleBrowZineWebLinkTextEnabled: true,
-    articleBrowZineWebLinkText       : '[THIS IS AN OVERRIDE FROM CDN custom.js] View Issue Contents',
+    articlePDFDownloadLinkText : '[THIS IS AN OVERRIDE FROM CDN custom.js] Download PDF',
 
-    articlePDFDownloadLinkEnabled: true,
-    articlePDFDownloadLinkText   : '[THIS IS AN OVERRIDE FROM CDN custom.js] Download PDF',
+    articleRetractionWatchText : '[THIS IS AN OVERRIDE FROM CDN custom.js] Retracted Article',
 
-    articleLinkEnabled: true,
-    articleLinkText   : 'Read Article',
-
-    printRecordsIntegrationEnabled: true,
-    showFormatChoice              : true,
-    showLinkResolverLink          : true,
-    enableLinkOptimizer           : true,
-
-    articleRetractionWatchEnabled: true,
-    articleRetractionWatchText   : '[THIS IS AN OVERRIDE FROM CDN custom.js] Retracted Article',
-
-    // See https://nyu-lib.monday.com/boards/765008773/pulses/4770873703/posts/2283414551
-    // for details on `unpaywallEmailAddressKey`.
-    unpaywallEmailAddressKey                               : 'fakeuser@nyu.edu',
-    articlePDFDownloadViaUnpaywallEnabled                  : true,
-    articlePDFDownloadViaUnpaywallText                     : '[THIS IS AN OVERRIDE FROM CDN custom.js] Download PDF (via Unpaywall)',
-    articleLinkViaUnpaywallEnabled                         : true,
-    articleLinkViaUnpaywallText                            : '[THIS IS AN OVERRIDE FROM CDN custom.js] Read Article (via Unpaywall)',
-    articleAcceptedManuscriptPDFViaUnpaywallEnabled        : true,
-    articleAcceptedManuscriptPDFViaUnpaywallText           : '[THIS IS AN OVERRIDE FROM CDN custom.js] Download PDF (Accepted Manuscript via Unpaywall)',
-    articleAcceptedManuscriptArticleLinkViaUnpaywallEnabled: true,
-    articleAcceptedManuscriptArticleLinkViaUnpaywallText   : '[THIS IS AN OVERRIDE FROM CDN custom.js] Read Article (Accepted Manuscript via Unpaywall)',
+    articlePDFDownloadViaUnpaywallText                   : '[THIS IS AN OVERRIDE FROM CDN custom.js] Download PDF (via Unpaywall)',
+    articleLinkViaUnpaywallText                          : '[THIS IS AN OVERRIDE FROM CDN custom.js] Read Article (via Unpaywall)',
+    articleAcceptedManuscriptPDFViaUnpaywallText         : '[THIS IS AN OVERRIDE FROM CDN custom.js] Download PDF (Accepted Manuscript via Unpaywall)',
+    articleAcceptedManuscriptArticleLinkViaUnpaywallText : '[THIS IS AN OVERRIDE FROM CDN custom.js] Read Article (Accepted Manuscript via Unpaywall)',
 }
 
 Object.assign( window.browzine, browzineConfig );
