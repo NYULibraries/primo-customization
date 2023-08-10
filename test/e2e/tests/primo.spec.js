defe16bfee23ca3fbbf5e47e80fdbe873b6ef65b
David <da70@nyu.edu>
2023-08-10 01:27:37 -0400

    test/e2e/: intial commit of *NON-VIABLE FIRST DRAFT -- FOR DEMONSTRATION PURPOSES ONLY!*
    - Basic working playwright setup for testing 01NYU_INST-NYU_DEV using HTML golden files.
    - Not viable as-is because the HTML for the tests cases and likely for all Primo VE page loads is not stable.  See test/e2e/tests/diffs/01NYU_INST-NYU_DEV/ reports after running the tests.
    - Golden files were generated with CDN equivalent of https://github.com/NYULibraries/primo-customization-cdn/commit/b75b41bcdcd64f9a35cf6c35c07f6e316bc557ba

---
+++ test/e2e/tests/primo.spec.js
@@ -0,0 +1,111 @@
+import * as fs from 'node:fs';
+
+import { execSync } from 'child_process';
+import { updateGoldenFiles, } from '../testutils/index.js';
+
+const beautifyHtml = require( 'js-beautify' ).html;
+const { test, expect } = require( '@playwright/test' );
+
+const view = process.env.VIEW;
+const vid = view.replaceAll( '-', ':' );
+
+const testCases = [
+    {
+        key         : 'libkey-article-search-art',
+        name        : 'LibKey for article search: "art"',
+        queryString : 'query=any,contains,art&tab=Unified_Slot&search_scope=ARTICLES&offset=0',
+    },
+    {
+        key         : 'home-page',
+        name        : 'Home page',
+        queryString : '',
+    },
+    {
+        key         : 'no-search-results',
+        name        : 'gasldfjlak===asgjlk&&&&!!!!',
+        queryString : 'query=any,contains,art&tab=Unified_Slot&search_scope=ARTICLES&offset=0',
+    },
+];
+
+for ( let i = 0; i < testCases.length; i++ ) {
+    const testCase = testCases[ i ];
+
+    test.describe( `${testCase.name}`, () => {
+        test.beforeEach( async ( { page } ) => {
+            let fullQueryString = `?vid=${ vid }`;
+            if ( testCase.queryString ) {
+                fullQueryString += `&${ testCase.queryString }`;
+            }
+            await page.goto( fullQueryString );
+        } );
+
+        test( 'page HTML matches expected', async ( { page } ) => {
+            // Clean actual/ and diffs/ files
+            // NOTE:
+            // We don't bother with error handling because these files get overwritten
+            // anyway, and if there were no previous files, or if a previous cleaning/reset
+            // script or process already deleted the previous files, we don't want the errors
+            // causing distraction.
+            // If deletion fails on existing files, there's a good chance there will
+            // be errors thrown later, which will then correctly fail the test.
+            const actualFile = `tests/actual/${ view }/${testCase.key}.html`;
+            try {
+                fs.unlinkSync( actualFile );
+            } catch ( error ) {
+            }
+            const diffFile = `tests/diffs/${ view }/${testCase.key}.txt`;
+            try {
+                fs.unlinkSync( diffFile );
+            } catch ( error ) {
+            }
+
+            // await page.waitForSelector( 'a:has-text("Download PDF (via Unpaywall)")' );
+            await page.waitForLoadState( 'load' );
+
+            const actual = beautifyHtml( await page.content() );
+
+            const goldenFile = `tests/golden/${ view }/${testCase.key}.html`;
+            if ( updateGoldenFiles() ) {
+                fs.writeFileSync( goldenFile, actual );
+
+                console.log( `Updated golden file ${goldenFile}` );
+
+                return;
+            }
+            const golden = beautifyHtml( fs.readFileSync( goldenFile, { encoding : 'utf8' } ) );
+
+            fs.writeFileSync( actualFile, actual );
+
+            const ok = actual === golden;
+
+            let message = `Actual HTML for "${testCase.name}" does not match expected HTML`;
+            if ( !ok ) {
+                const command = `diff ${goldenFile} ${actualFile} | tee ${diffFile}`;
+                let diffOutput;
+                try {
+                    diffOutput = new TextDecoder().decode( execSync( command ) );
+                    message += `
+
+======= BEGIN DIFF OUTPUT ========
+${diffOutput}
+======== END DIFF OUTPUT =========
+
+[Recorded in diff file: ${diffFile}]`;
+                } catch ( e ) {
+                    // `diff` command failed to create the diff file.
+                    message += `  Diff command \`${command}\` failed:
+
+${e.stderr.toString()}`;
+                }
+            }
+
+            expect( ok, message ).toBe( true );
+        } );
+    } )
+}
+
+
+
+
+
+
---
+++ test/e2e/testutils/index.js
@@ -0,0 +1,12 @@
+// NOTE: it's current not possible to use a custom flag like `--update-golden-files`
+// with `playwright`:
+// "[Feature] Add support for test.each / describe.each #7036"
+// https://github.com/microsoft/playwright/issues/7036
+function updateGoldenFiles() {
+    return process.env.UPDATE_GOLDEN_FILES &&
+           process.env.UPDATE_GOLDEN_FILES.toLowerCase() !== 'false';
+}
+
+export {
+    updateGoldenFiles,
+};
