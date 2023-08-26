541e3a04879a13a252ffddb9124905b5d63c4d33
David <da70@nyu.edu>
2023-08-26 14:47:28 -0400

    libkey.spec.js: test a randomly selected LibKey link for correct new tab opening on click

--- test/e2e/tests/libkey.spec.js
+++ test/e2e/tests/libkey.spec.js
@@ -36,32 +36,74 @@ for ( let i = 0; i < testCases.length; i++ ) {
             await page.goto( fullQueryString );
         } );
 
-        test( 'has LibKey links', async ( { page } ) => {
+        test( 'has at least 1 clickable LibKey link', async ( { page } ) => {
             // TODO: Find a better waitFor condition.
             await page.waitForTimeout( 5_000 );
 
-            let libkeyLinksFound = false;
+            let libKeyLinksFound = false;
+            let randomLibKeyLinkTestResult = {
+                result: false,
+                linkHref: null,
+                newPageUrl: null,
+            };
             for ( let i = 0; i < LIBKEY_LINK_SELECTORS.length; i++ ) {
-                const libkeyLinksCount = await page.locator( LIBKEY_LINK_SELECTORS[ i ] ).count();
+                const libKeyLinks = await page.locator( LIBKEY_LINK_SELECTORS[ i ] ).all();
 
-                if ( libkeyLinksCount > 0 ) {
-                    libkeyLinksFound = true;
+                if ( libKeyLinks.length > 0 ) {
+                    libKeyLinksFound = true;
+
+                    // Since we can't make a reproducible LibKey link click test
+                    // due to the fact that we are testing live search results
+                    // which are not stable, we might as well increase our coverage
+                    // by selecting a link at random each time we test.
+                    await randomLibKeyLinkTest(
+                        page,
+                        libKeyLinks,
+                        randomLibKeyLinkTestResult
+                    );
 
                     break;
                 }
             }
 
             expect(
-                libkeyLinksFound,
+                libKeyLinksFound,
                 'No elements found matching any of the following: ' +
                     LIBKEY_LINK_SELECTORS.join( ', ' )
+            ).toBe( true );
+
+            expect(
+                randomLibKeyLinkTestResult.result,
+                `Randomly selected LibKey link with href "${ randomLibKeyLinkTestResult.linkHref }"` +
+                ` did not correctly open a new tab.` +
+                (
+                    randomLibKeyLinkTestResult.newPageUrl ?
+                    ` New tab loaded with incorrect URL "${ randomLibKeyLinkTestResult.newPageUrl }".` :
+                    ''
+                )
             ).toBe( true );
         } );
     } );
 }
 
+async function randomLibKeyLinkTest( page, libKeyLinks, testResult ) {
+    // Test a random LibKey link to see if clicking on it loads
+    // the right URL in a new tab.
+    const randomLibKeyLink =
+        libKeyLinks[ Math.floor( Math.random() * libKeyLinks.length ) ]
 
+    testResult.linkHref = await randomLibKeyLink.getAttribute( 'href' );
 
+    // https://playwright.dev/docs/pages#handling-new-pages
+    // Start waiting for new page before clicking. Note no await.
+    const pagePromise = page.context().waitForEvent( 'page' );
+    await randomLibKeyLink.click();
+    const newPage = await pagePromise;
+    await newPage.waitForLoadState();
 
+    testResult.newPageUrl = await newPage.url();
 
-
+    if ( testResult.newPageUrl === testResult.linkHref ) {
+        testResult.result = true;
+    }
+}
