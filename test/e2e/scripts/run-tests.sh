d2c266fcde66bffa5f9e383bd4b05b0a4c9f20ce
David <da70@nyu.edu>
2023-08-28 00:34:00 -0400

    test/e2e/scripts/run-tests.sh: get VIEW from environtment, not from first arg to script

--- test/e2e/scripts/run-tests.sh
+++ test/e2e/scripts/run-tests.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+# Mandatory environment variables: VIEW
+# Optional environment variables: UPDATE_GOLDEN_FILES
+
 ROOT=$( cd "$(dirname "$0")" || exit 1; cd ..; pwd -P )
 
-VIEW=$1
-
-if [ -z $VIEW ]; then
+if [ -z "$VIEW" ]; then
     echo >&2 "Please specify a view to test."
     exit 1
 fi
@@ -13,19 +14,19 @@ TEST_VIEW_DIR=$ROOT/tests
 ACTUAL_DIR=$TEST_VIEW_DIR/actual/$VIEW
 DIFFS_DIR=$TEST_VIEW_DIR/diffs/$VIEW
 
-if [ ! -d $ACTUAL_DIR ]; then
+if [ ! -d "$ACTUAL_DIR" ]; then
     echo >&2 "Actual dir $ACTUAL_DIR/ does not exist."
     exit 1
 fi
 
-if [ ! -d $DIFFS_DIR ]; then
+if [ ! -d "$DIFFS_DIR" ]; then
     echo >&2 "Diffs dir $DIFFS_DIR/ does not exist."
     exit 1
 fi
 
 # Clean
-rm -rf ${ACTUAL_DIR:?}/*
-rm -rf ${DIFFS_DIR:?}/*
+rm -rf "${ACTUAL_DIR:?}"/*
+rm -rf "${DIFFS_DIR:?}"/*
 
 # Run tests
-VIEW=$VIEW yarn playwright test
+yarn playwright test
