dc7700748afe379ebf35f248cfe74e51c035e8b8
David <da70@nyu.edu>
2023-09-11 20:37:25 -0400

    scripts/push-images-to-quay.sh: add primo-customization-e2e

--- scripts/push-images-to-quay.sh
+++ scripts/push-images-to-quay.sh
@@ -15,3 +15,11 @@ docker tag primo-customization-devenv quay.io/nyulibraries/primo-customization-d
 docker push quay.io/nyulibraries/primo-customization-devenv:latest
 docker push quay.io/nyulibraries/primo-customization-devenv:${BRANCH_NO_SLASH}
 docker push quay.io/nyulibraries/primo-customization-devenv:${BRANCH_NO_SLASH}-${CIRCLE_SHA1}
+
+docker tag primo-customization-e2e quay.io/nyulibraries/primo-customization-e2e:latest
+docker tag primo-customization-e2e quay.io/nyulibraries/primo-customization-e2e:${BRANCH_NO_SLASH}
+docker tag primo-customization-e2e quay.io/nyulibraries/primo-customization-e2e:${BRANCH_NO_SLASH}-${CIRCLE_SHA1}
+
+docker push quay.io/nyulibraries/primo-customization-e2e:latest
+docker push quay.io/nyulibraries/primo-customization-e2e:${BRANCH_NO_SLASH}
+docker push quay.io/nyulibraries/primo-customization-e2e:${BRANCH_NO_SLASH}-${CIRCLE_SHA1}
