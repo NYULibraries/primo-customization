062cd17911ca6e9d50541eab4374ca0aa78d3282
David <da70@nyu.edu>
2023-09-11 20:36:01 -0400

    scripts/push-images-to-quay.sh: alphasort by image name

--- scripts/push-images-to-quay.sh
+++ scripts/push-images-to-quay.sh
@@ -1,13 +1,5 @@
 #!/bin/sh -ex
 
-docker tag primo-customization-devenv quay.io/nyulibraries/primo-customization-devenv:latest
-docker tag primo-customization-devenv quay.io/nyulibraries/primo-customization-devenv:${BRANCH_NO_SLASH}
-docker tag primo-customization-devenv quay.io/nyulibraries/primo-customization-devenv:${BRANCH_NO_SLASH}-${CIRCLE_SHA1}
-
-docker push quay.io/nyulibraries/primo-customization-devenv:latest
-docker push quay.io/nyulibraries/primo-customization-devenv:${BRANCH_NO_SLASH}
-docker push quay.io/nyulibraries/primo-customization-devenv:${BRANCH_NO_SLASH}-${CIRCLE_SHA1}
-
 docker tag primo-customization-cdn-server quay.io/nyulibraries/primo-customization-cdn-server:latest
 docker tag primo-customization-cdn-server quay.io/nyulibraries/primo-customization-cdn-server:${BRANCH_NO_SLASH}
 docker tag primo-customization-cdn-server quay.io/nyulibraries/primo-customization-cdn-server:${BRANCH_NO_SLASH}-${CIRCLE_SHA1}
@@ -15,3 +7,11 @@ docker tag primo-customization-cdn-server quay.io/nyulibraries/primo-customizati
 docker push quay.io/nyulibraries/primo-customization-cdn-server:latest
 docker push quay.io/nyulibraries/primo-customization-cdn-server:${BRANCH_NO_SLASH}
 docker push quay.io/nyulibraries/primo-customization-cdn-server:${BRANCH_NO_SLASH}-${CIRCLE_SHA1}
+
+docker tag primo-customization-devenv quay.io/nyulibraries/primo-customization-devenv:latest
+docker tag primo-customization-devenv quay.io/nyulibraries/primo-customization-devenv:${BRANCH_NO_SLASH}
+docker tag primo-customization-devenv quay.io/nyulibraries/primo-customization-devenv:${BRANCH_NO_SLASH}-${CIRCLE_SHA1}
+
+docker push quay.io/nyulibraries/primo-customization-devenv:latest
+docker push quay.io/nyulibraries/primo-customization-devenv:${BRANCH_NO_SLASH}
+docker push quay.io/nyulibraries/primo-customization-devenv:${BRANCH_NO_SLASH}-${CIRCLE_SHA1}
