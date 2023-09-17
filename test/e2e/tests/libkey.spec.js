ee54a3262514e411072c8102342fcdf27fe147d8
David <da70@nyu.edu>
2023-09-17 12:31:22 -0400

    libkey.spec.js: extract `testHasAClickableLibKeyLink` helper function

--- test/e2e/tests/libkey.spec.js
+++ test/e2e/tests/libkey.spec.js
@@ -37,81 +37,85 @@ for ( let i = 0; i < testCases.length; i++ ) {
         } );
 
         test( 'has a clickable LibKey link', async ( { page } ) => {
-            // Tests running in container sometimes take longer and require a
-            // higher timeout value.
-            if ( process.env.IN_CONTAINER ) {
-                // This test once timed out in CircleCI at 3 minutes:
-                // https://app.circleci.com/pipelines/github/NYULibraries/primo-customization/127/workflows/658dc7cb-0d06-4b9d-acb3-51a6c96c9f48/jobs/110
-                // ...so setting default timeout to 5 minutes to be on the safe side.
-                test.setTimeout( 300_000 );
-            }
-
-            const waitForLibKeyLinksFunction = ( libKeySelectors ) => {
-                let result = false;
-
-                for ( let i = 0; i < libKeySelectors.length; i++ ) {
-                    if ( !!document.querySelector( libKeySelectors[ i ] ) ) {
-                        result = true;
-
-                        break;
-                    }
-                }
-
-                return result;
-            }
-
-            // Continuously search for at least one element matching any selector
-            // in LIBKEY_LINK_SELECTORS.
-            // Note that we can't save the element for later when we test click
-            // behavior because `waitForFunction` functions run in the browser,
-            // not in Playwright's Node context.
-            await page.waitForFunction( waitForLibKeyLinksFunction, LIBKEY_LINK_SELECTORS );
-
-            // Since we can't make a reproducible LibKey link click test
-            // due to the fact that we are testing live search results
-            // which are not stable, we might as well increase our coverage
-            // by selecting a link at random each time we test.
-            // As mentioned above, we can't re-use the link found by
-            // `waitForLibKeyLinksFunction`, so we have to loop through and
-            // find one again.  Despite the repetition, it is advantageous to do
-            // the previous `waitFor` test because we can not be assured that
-            // when we use `page.locator` to find LibKey links we do not have to
-            // specify long timeouts, because if `page.locator` fails, we can
-            // be confident that it's due to the absence of links matching that
-            // selector being returned by the LibKey code, and not due to LibKey
-            // links not having to appear yet.  If we didn't have this assurance,
-            // we'd have to allow each `page.locator` call to run a long time,
-            // or do an awkward warm-up step first.
-            let libKeyLinks = [];
-            for ( let i = 0; i < LIBKEY_LINK_SELECTORS.length; i++ ) {
-                const links = await page.locator( LIBKEY_LINK_SELECTORS[ i ] ).all();
-
-                libKeyLinks = [ ...libKeyLinks, ...links ];
-            }
-
-            let randomLibKeyLinkTestResult = {
-                result: false,
-                linkHref: null,
-                newPageUrl: null,
-            };
-            await randomLibKeyLinkTest(
-                page,
-                libKeyLinks,
-                randomLibKeyLinkTestResult,
-            );
-
-            expect(
-                randomLibKeyLinkTestResult.result,
-                `Randomly selected LibKey link with href "${ randomLibKeyLinkTestResult.linkHref }"` +
-                ` did not correctly open a new tab.` +
-                (
-                    randomLibKeyLinkTestResult.newPageUrl ?
-                    ` New tab loaded with incorrect URL "${ randomLibKeyLinkTestResult.newPageUrl }".` :
-                    ''
-                )
-            ).toBe( true );
+            await testHasAClickableLibKeyLink( page );
         } );
     } );
+}
+
+async function testHasAClickableLibKeyLink( page ) {
+    // Tests running in container sometimes take longer and require a
+    // higher timeout value.
+    if ( process.env.IN_CONTAINER ) {
+        // This test once timed out in CircleCI at 3 minutes:
+        // https://app.circleci.com/pipelines/github/NYULibraries/primo-customization/127/workflows/658dc7cb-0d06-4b9d-acb3-51a6c96c9f48/jobs/110
+        // ...so setting default timeout to 5 minutes to be on the safe side.
+        test.setTimeout( 300_000 );
+    }
+
+    const waitForLibKeyLinksFunction = ( libKeySelectors ) => {
+        let result = false;
+
+        for ( let i = 0; i < libKeySelectors.length; i++ ) {
+            if ( !!document.querySelector( libKeySelectors[ i ] ) ) {
+                result = true;
+
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    // Continuously search for at least one element matching any selector
+    // in LIBKEY_LINK_SELECTORS.
+    // Note that we can't save the element for later when we test click
+    // behavior because `waitForFunction` functions run in the browser,
+    // not in Playwright's Node context.
+    await page.waitForFunction( waitForLibKeyLinksFunction, LIBKEY_LINK_SELECTORS );
+
+    // Since we can't make a reproducible LibKey link click test
+    // due to the fact that we are testing live search results
+    // which are not stable, we might as well increase our coverage
+    // by selecting a link at random each time we test.
+    // As mentioned above, we can't re-use the link found by
+    // `waitForLibKeyLinksFunction`, so we have to loop through and
+    // find one again.  Despite the repetition, it is advantageous to do
+    // the previous `waitFor` test because we can not be assured that
+    // when we use `page.locator` to find LibKey links we do not have to
+    // specify long timeouts, because if `page.locator` fails, we can
+    // be confident that it's due to the absence of links matching that
+    // selector being returned by the LibKey code, and not due to LibKey
+    // links not having to appear yet.  If we didn't have this assurance,
+    // we'd have to allow each `page.locator` call to run a long time,
+    // or do an awkward warm-up step first.
+    let libKeyLinks = [];
+    for ( let i = 0; i < LIBKEY_LINK_SELECTORS.length; i++ ) {
+        const links = await page.locator( LIBKEY_LINK_SELECTORS[ i ] ).all();
+
+        libKeyLinks = [ ...libKeyLinks, ...links ];
+    }
+
+    let randomLibKeyLinkTestResult = {
+        result: false,
+        linkHref: null,
+        newPageUrl: null,
+    };
+    await randomLibKeyLinkTest(
+        page,
+        libKeyLinks,
+        randomLibKeyLinkTestResult,
+    );
+
+    expect(
+        randomLibKeyLinkTestResult.result,
+        `Randomly selected LibKey link with href "${ randomLibKeyLinkTestResult.linkHref }"` +
+        ` did not correctly open a new tab.` +
+        (
+            randomLibKeyLinkTestResult.newPageUrl ?
+            ` New tab loaded with incorrect URL "${ randomLibKeyLinkTestResult.newPageUrl }".` :
+            ''
+        )
+    ).toBe( true );
 }
 
 async function randomLibKeyLinkTest( page, libKeyLinks, testResult ) {
