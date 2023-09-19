889ee2ff3230c7c3bc5b9cb3359a76b650effa4c
David <da70@nyu.edu>
2023-09-18 20:44:31 -0400

    test/e2e/tests/static.spec.js: bring back `test.slow()` in containers, and pull up to `test.describe()`

--- test/e2e/tests/static.spec.js
+++ test/e2e/tests/static.spec.js
@@ -29,6 +29,14 @@ for ( let i = 0; i < testCases.length; i++ ) {
     const testCase = testCases[ i ];
 
     test.describe( `${ view }: ${ testCase.name }`, () => {
+        // Tests running in container sometimes take longer and require a
+        // higher timeout value.  These tests have timed out in containers in
+        // both `test.beforeEach()` and the test itself, so we need to increase
+        // the timeout for everything in `test.describe()`.
+        if ( process.env.IN_CONTAINER ) {
+            test.slow();
+        }
+
         test.beforeEach( async ( { page } ) => {
             let fullQueryString = `?vid=${ vid }`;
             if ( testCase.queryString ) {
