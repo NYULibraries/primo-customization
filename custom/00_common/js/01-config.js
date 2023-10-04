038897efbe223afe181b749f4c5b3527a720e2f5
David <da70@nyu.edu>
2023-10-04 13:59:04 -0400

    custom/00_common/js/01-config.js: create `window.nyulibraries` and attach `cdnUrl` to it

--- custom/00_common/js/01-config.js
+++ custom/00_common/js/01-config.js
@@ -8,6 +8,19 @@ const cdnUrl = getCdnUrl( vid );
 
 console.log( `[DEBUG] cdnUrl = ${ cdnUrl }` );
 
+// All the code in our customization package is run inside an IIFE (Immediately
+// Invoked Function Expression), which means any variables defined here are not
+// accessible to the CDN JS code.  The only way for the CDN JS code to know the
+// CDN URL without duplicating `getCdnUrl()` there is to attach it to an object
+// it has access to, or to inject it in a <script> or DOM element on the page.
+// We choose to attach it to the global `window` object since we are already
+// allowing the Third Iron code to attach the `browzine` object to `window` for
+// LibKey functionality, and it seems safe enough using the `nyulibraries`
+// namespace.
+window.nyulibraries = {
+    cdnUrl,
+};
+
 // This is necessary to allow the `templateURL` method to fetch cross-domain
 // from the CDN.
 app.config( function ( $sceDelegateProvider ) {
