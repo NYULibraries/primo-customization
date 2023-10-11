986c4f102e640e9733cdbb24a4885f5dc292f5f2
David <da70@nyu.edu>
2023-10-11 10:56:11 -0400

    test/e2e/tests/view-config/libkey/01NYU_INST-TESTWS01.js: get test cases from prod

--- test/e2e/tests/view-config/libkey/01NYU_INST-TESTWS01.js
+++ test/e2e/tests/view-config/libkey/01NYU_INST-TESTWS01.js
@@ -1,45 +1,6 @@
-const testCases = [
-    // === General searches: potential risk of false positives due to search ranking volatility
+import { getViewConfig } from '../../../testutils';
 
-    {
-        key         : 'art',
-        name        : 'Art',
-        queryString : 'query=any,contains,art&tab=Unified_Slot&search_scope=ARTICLES&offset=0',
-    },
-    {
-        key                                : 'art',
-        name                               : 'Art',
-        queryString                        : 'query=any,contains,art&tab=Unified_Slot&search_scope=ARTICLES&offset=0',
-        browzinePrimoAdapterExecutionDelay : 10_000,
-    },
-
-    // === Targeted `docid` searches: not subject to search ranking volatility
-    //
-    // The test cases below are targeted `docid` searches provided by KARMS DAI.
-    // DAI determined that based on their permalink history, these records are
-    // likely to be stable over time with regard to inclusion of LibKey links.
-
-    {
-        key         : 'docid-cdi_crossref_primary_10_1080_14672715_2021_1888307',
-        name        : 'docid: cdi_crossref_primary_10_1080_14672715_2021_1888307',
-        queryString : 'query=any,contains,cdi_crossref_primary_10_1080_14672715_2021_1888307&tab=Unified_Slot&search_scope=DN_and_CI&offset=0',
-    },
-    {
-        key         : 'docid-cdi_informaworld_taylorfrancis_310_1080_15596893_2022_2116781',
-        name        : 'docid: cdi_informaworld_taylorfrancis_310_1080_15596893_2022_2116781',
-        queryString : 'query=any,contains,cdi_informaworld_taylorfrancis_310_1080_15596893_2022_2116781&tab=Unified_Slot&search_scope=DN_and_CI&offset=0',
-    },
-    {
-        key         : 'docid-cdi_proquest_journals_2358492548',
-        name        : 'docid: cdi_proquest_journals_2358492548',
-        queryString : 'query=any,contains,cdi_proquest_journals_2358492548&tab=Unified_Slot&search_scope=DN_and_CI&offset=0',
-    },
-    {
-        key         : 'docid-cdi_cleo_primary_oai_revues_org_chinaperspectives_7327',
-        name        : 'docid: cdi_cleo_primary_oai_revues_org_chinaperspectives_7327',
-        queryString : 'query=any,contains,cdi_cleo_primary_oai_revues_org_chinaperspectives_7327&tab=Unified_Slot&search_scope=DN_and_CI&offset=0',
-    },
-];
+const testCases = getViewConfig( 'libkey', '01NYU_INST-NYU' ).testCases;
 
 export {
     testCases,
