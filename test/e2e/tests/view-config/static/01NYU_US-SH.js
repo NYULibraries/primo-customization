90b0b5cfa7dcb0be4cb6739c219f16c3757f41ca
David <da70@nyu.edu>
2023-10-11 16:46:17 -0400

    test/e2e/tests/view-config/static/: commented out `no-search-results` test cases for AD, NYSH, and SH -- customizations have not been put in place yet, so tests fail due to `waitForSelector` `prm-no-search-result-after` being hidden

--- test/e2e/tests/view-config/static/01NYU_US-SH.js
+++ test/e2e/tests/view-config/static/01NYU_US-SH.js
@@ -6,13 +6,15 @@ const testCases = [
         elementToTest   : 'prm-static',
         waitForSelector : 'md-card[ data-cy="home-need-help" ]',
     },
-    {
-        key             : 'no-search-results',
-        name            : 'gasldfjlak===asgjlk&&&&!!!!',
-        queryString     : 'query=any,contains,gasldfjlak%3D%3D%3Dasgjlk%26%26%26%26!!!!&tab=default_slot&search_scope=CI_NYUSH_NYU&offset=0',
-        elementToTest   : 'prm-no-search-result',
-        waitForSelector : 'prm-no-search-result-after',
-    },
+    // TODO: Re-enable if/when customization has been implemented.  Until then,
+    //       this test will fail due to `prm-no-search-result-after` being hidden.
+    // {
+    //     key             : 'no-search-results',
+    //     name            : 'gasldfjlak===asgjlk&&&&!!!!',
+    //     queryString     : 'query=any,contains,gasldfjlak%3D%3D%3Dasgjlk%26%26%26%26!!!!&tab=default_slot&search_scope=CI_NYUSH_NYU&offset=0',
+    //     elementToTest   : 'prm-no-search-result',
+    //     waitForSelector : 'prm-no-search-result-after',
+    // },
 ];
 
 export {
