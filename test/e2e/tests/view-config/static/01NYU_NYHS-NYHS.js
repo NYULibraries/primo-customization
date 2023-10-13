593423fa2c95ea492c66ee6a1af954eb94d1ea2d
David <da70@nyu.edu>
2023-10-13 12:39:35 -0400

    test/e2e/tests/view-config/static/01NYU_NYHS-NYHS.js: remove commented out `no-search-results` test, as it might never be customized (it is a non-NYU view)

--- test/e2e/tests/view-config/static/01NYU_NYHS-NYHS.js
+++ test/e2e/tests/view-config/static/01NYU_NYHS-NYHS.js
@@ -6,15 +6,6 @@ const testCases = [
         elementToTest   : 'prm-static',
         waitForSelector : 'md-card[ data-cy="home-additional-options" ]',
     },
-    // TODO: Re-enable if/when customization has been implemented.  Until then,
-    //       this test will fail due to `prm-no-search-result-after` being hidden.
-    // {
-    //     key             : 'no-search-results',
-    //     name            : 'gasldfjlak===asgjlk&&&&!!!!',
-    //     queryString     : 'query=any,contains,gasldfjlak%3D%3D%3Dasgjlk%26%26%26%26!!!!&tab=unified_slot&search_scope=MyInstitution&offset=0',
-    //     elementToTest   : 'prm-no-search-result',
-    //     waitForSelector : 'prm-no-search-result-after',
-    // },
 ];
 
 export {
